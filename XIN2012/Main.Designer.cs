ed471278bb1f941caf236630d1bd552ca00471b6
foxliu36 <fox.liu36@gmail.com>
2013-12-24 22:19:37 +0800

    first

---
+++ XIN2012/Main.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace XIN2012
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.基本資料ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.商品基本資料維護ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.廠商基本資料維護ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.客戶基本資料維護ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.商品基本資料表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.客戶基本資料表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.結束系統ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.進貨作業ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.出貨作業ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.庫存管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.系統管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.使用者代碼維護ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.程式使用權限維護ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.使用者執行權限維護ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.使用者密碼變更ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.miAboutBox = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.基本資料ToolStripMenuItem,
+            this.進貨作業ToolStripMenuItem,
+            this.出貨作業ToolStripMenuItem,
+            this.庫存管理ToolStripMenuItem,
+            this.系統管理ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1004, 27);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 基本資料ToolStripMenuItem
+            // 
+            this.基本資料ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.商品基本資料維護ToolStripMenuItem,
+            this.廠商基本資料維護ToolStripMenuItem,
+            this.客戶基本資料維護ToolStripMenuItem,
+            this.商品基本資料表ToolStripMenuItem,
+            this.客戶基本資料表ToolStripMenuItem,
+            this.結束系統ToolStripMenuItem});
+            this.基本資料ToolStripMenuItem.Name = "基本資料ToolStripMenuItem";
+            this.基本資料ToolStripMenuItem.Size = new System.Drawing.Size(81, 23);
+            this.基本資料ToolStripMenuItem.Text = "基本資料";
+            // 
+            // 商品基本資料維護ToolStripMenuItem
+            // 
+            this.商品基本資料維護ToolStripMenuItem.Name = "商品基本資料維護ToolStripMenuItem";
+            this.商品基本資料維護ToolStripMenuItem.Size = new System.Drawing.Size(198, 24);
+            this.商品基本資料維護ToolStripMenuItem.Text = "商品基本資料維護";
+            // 
+            // 廠商基本資料維護ToolStripMenuItem
+            // 
+            this.廠商基本資料維護ToolStripMenuItem.Name = "廠商基本資料維護ToolStripMenuItem";
+            this.廠商基本資料維護ToolStripMenuItem.Size = new System.Drawing.Size(198, 24);
+            this.廠商基本資料維護ToolStripMenuItem.Text = "廠商基本資料維護";
+            // 
+            // 客戶基本資料維護ToolStripMenuItem
+            // 
+            this.客戶基本資料維護ToolStripMenuItem.Name = "客戶基本資料維護ToolStripMenuItem";
+            this.客戶基本資料維護ToolStripMenuItem.Size = new System.Drawing.Size(198, 24);
+            this.客戶基本資料維護ToolStripMenuItem.Text = "客戶基本資料維護";
+            // 
+            // 商品基本資料表ToolStripMenuItem
+            // 
+            this.商品基本資料表ToolStripMenuItem.Name = "商品基本資料表ToolStripMenuItem";
+            this.商品基本資料表ToolStripMenuItem.Size = new System.Drawing.Size(198, 24);
+            this.商品基本資料表ToolStripMenuItem.Text = "商品基本資料表";
+            // 
+            // 客戶基本資料表ToolStripMenuItem
+            // 
+            this.客戶基本資料表ToolStripMenuItem.Name = "客戶基本資料表ToolStripMenuItem";
+            this.客戶基本資料表ToolStripMenuItem.Size = new System.Drawing.Size(198, 24);
+            this.客戶基本資料表ToolStripMenuItem.Text = "客戶基本資料表";
+            // 
+            // 結束系統ToolStripMenuItem
+            // 
+            this.結束系統ToolStripMenuItem.Name = "結束系統ToolStripMenuItem";
+            this.結束系統ToolStripMenuItem.Size = new System.Drawing.Size(198, 24);
+            this.結束系統ToolStripMenuItem.Text = "結束系統";
+            // 
+            // 進貨作業ToolStripMenuItem
+            // 
+            this.進貨作業ToolStripMenuItem.Name = "進貨作業ToolStripMenuItem";
+            this.進貨作業ToolStripMenuItem.Size = new System.Drawing.Size(81, 23);
+            this.進貨作業ToolStripMenuItem.Text = "進貨作業";
+            // 
+            // 出貨作業ToolStripMenuItem
+            // 
+            this.出貨作業ToolStripMenuItem.Name = "出貨作業ToolStripMenuItem";
+            this.出貨作業ToolStripMenuItem.Size = new System.Drawing.Size(81, 23);
+            this.出貨作業ToolStripMenuItem.Text = "出貨作業";
+            // 
+            // 庫存管理ToolStripMenuItem
+            // 
+            this.庫存管理ToolStripMenuItem.Name = "庫存管理ToolStripMenuItem";
+            this.庫存管理ToolStripMenuItem.Size = new System.Drawing.Size(81, 23);
+            this.庫存管理ToolStripMenuItem.Text = "庫存管理";
+            // 
+            // 系統管理ToolStripMenuItem
+            // 
+            this.系統管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.使用者代碼維護ToolStripMenuItem,
+            this.程式使用權限維護ToolStripMenuItem,
+            this.使用者執行權限維護ToolStripMenuItem,
+            this.使用者密碼變更ToolStripMenuItem,
+            this.miAboutBox});
+            this.系統管理ToolStripMenuItem.Name = "系統管理ToolStripMenuItem";
+            this.系統管理ToolStripMenuItem.Size = new System.Drawing.Size(81, 23);
+            this.系統管理ToolStripMenuItem.Text = "系統管理";
+            // 
+            // 使用者代碼維護ToolStripMenuItem
+            // 
+            this.使用者代碼維護ToolStripMenuItem.Name = "使用者代碼維護ToolStripMenuItem";
+            this.使用者代碼維護ToolStripMenuItem.Size = new System.Drawing.Size(213, 24);
+            this.使用者代碼維護ToolStripMenuItem.Text = "使用者代碼維護";
+            // 
+            // 程式使用權限維護ToolStripMenuItem
+            // 
+            this.程式使用權限維護ToolStripMenuItem.Name = "程式使用權限維護ToolStripMenuItem";
+            this.程式使用權限維護ToolStripMenuItem.Size = new System.Drawing.Size(213, 24);
+            this.程式使用權限維護ToolStripMenuItem.Text = "程式使用權限維護";
+            // 
+            // 使用者執行權限維護ToolStripMenuItem
+            // 
+            this.使用者執行權限維護ToolStripMenuItem.Name = "使用者執行權限維護ToolStripMenuItem";
+            this.使用者執行權限維護ToolStripMenuItem.Size = new System.Drawing.Size(213, 24);
+            this.使用者執行權限維護ToolStripMenuItem.Text = "使用者執行權限維護";
+            // 
+            // 使用者密碼變更ToolStripMenuItem
+            // 
+            this.使用者密碼變更ToolStripMenuItem.Name = "使用者密碼變更ToolStripMenuItem";
+            this.使用者密碼變更ToolStripMenuItem.Size = new System.Drawing.Size(213, 24);
+            this.使用者密碼變更ToolStripMenuItem.Text = "使用者密碼變更";
+            // 
+            // miAboutBox
+            // 
+            this.miAboutBox.Name = "miAboutBox";
+            this.miAboutBox.Size = new System.Drawing.Size(213, 24);
+            this.miAboutBox.Text = "關於進銷存管理系統";
+            this.miAboutBox.Click += new System.EventHandler(this.miAbout_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 509);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1004, 24);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(158, 19);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 533);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "視窗";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 基本資料ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 商品基本資料維護ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 廠商基本資料維護ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 客戶基本資料維護ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 商品基本資料表ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 客戶基本資料表ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 結束系統ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 進貨作業ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 出貨作業ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 庫存管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 系統管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 使用者代碼維護ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 程式使用權限維護ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 使用者執行權限維護ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 使用者密碼變更ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem miAboutBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
